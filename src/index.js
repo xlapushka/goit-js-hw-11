325c68206fef803887d323dca650287bbf0e9d64
Yuliia Hubarenko <xlapushka@ukr.net>
2023-06-19 12:15:42 +0300

    all are done exept async/await, aios

--- src/index.js
+++ src/index.js
@@ -1,29 +1,48 @@
 const axios = require('axios').default;
 import Notiflix from 'notiflix';
 
+
 const BASE_URL ='https://pixabay.com/api/';
 const API_KEY = '37474712-2dac32f03878bab58bae809d9';
 
-let keyWord='';
 let per_page = 40;
 let currentPage = 1;
+let keyWord = '';
 
 const refs = {
-  submit : document.querySelector('#search-form'),
+  formSubmit : document.querySelector('#search-form'),
+  formSubmitInput : document.querySelector('#search-form input'),
   list : document.querySelector('.js-list'),
   loadMoreBtn : document.querySelector('.btn')
 };
 
-refs.list.innerHTML = '';
 refs.loadMoreBtn.addEventListener('click', loadMore);
-refs.submit.addEventListener('submit', impactWord);
-// document.querySelector('input[name=nameOfradio]:checked').value;
+refs.formSubmit.addEventListener('submit', impactWord);
+
+refs.list.innerHTML = '';
 
 function impactWord(evt) {
   evt.preventDefault();
-  keyWord = keyWord = evt.currentTarget.elements.searchQuery.value;
-  console.log(keyWord)
+  // ====================name у submit = searchQuery, додається в elements у currentTarget==================
+  keyWord = evt.currentTarget.elements.searchQuery.value;
+  refs.list.innerHTML = '';
+  currentPage = 1;
+
+  getImages(currentPage, keyWord)
+  .then(resp => {
+    if (currentPage <= Math.floor(resp.total/per_page)) {
+      refs.loadMoreBtn.hidden = false;
+    };
+
+    if (resp.totalHits > 0) {
+      Notiflix.Notify.success(`Hooray! We found ${resp.totalHits} images.`);
+    };
+    
+    refs.list.insertAdjacentHTML('beforeend', createMarkUp(resp.hits));
+  })
+  .catch(err => console.log(err));
 } 
+
 
 // $.getJSON(URL, function(data){
 // if (parseInt(data.totalHits) > 0)
@@ -33,7 +52,7 @@ function impactWord(evt) {
 // });
 
 
-function getImages(page=1) { 
+function getImages(page=1, keyWord) { 
   const params = new URLSearchParams ({
     image_type : 'photo',
     orientation : 'horizontal',
@@ -43,10 +62,10 @@ function getImages(page=1) {
   const options = {
     method: 'GET',
     headers: {
-
     }
   };
   
+
   return fetch(`${BASE_URL}?key=${API_KEY}&q=${keyWord}&${params}&page=${page}&per_page=${per_page}`)
     .then(resp => {
       if (!resp.ok) {
@@ -58,18 +77,18 @@ function getImages(page=1) {
     .catch(err => console.log(err))
 }
 
-getImages()
-  .then(resp => {
-    if (currentPage <= Math.floor(resp.total/per_page)) {
-      refs.loadMoreBtn.hidden = false;
-    };
+// getImages()
+//   .then(resp => {
+//     if (currentPage <= Math.floor(resp.total/per_page)) {
+//       refs.loadMoreBtn.hidden = false;
+//     };
 
-    if (resp.totalHits > 0) {
-      Notiflix.Notify.success(`Hooray! We found ${resp.totalHits} images.`);
-    };
-    refs.list.insertAdjacentHTML('beforeend', createMarkUp(resp.hits));
-  })
-  .catch(err => console.log(err));
+//     if (resp.totalHits > 0) {
+//       Notiflix.Notify.success(`Hooray! We found ${resp.totalHits} images.`);
+//     };
+//     refs.list.insertAdjacentHTML('beforeend', createMarkUp(resp.hits));
+//   })
+//   .catch(err => console.log(err));
 
 function createMarkUp(arr) {
   if (arr.length === 0) {
@@ -108,8 +127,10 @@ function createMarkUp(arr) {
 } 
 
 function loadMore() {
+  
   currentPage += 1;
-  getImages(currentPage)
+
+  getImages(currentPage, keyWord)
   .then(resp => {
     if (currentPage > Math.floor(resp.total/per_page)) {
       Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
